c1c5a8e572db212ead842239f3015f7403d266aa
Cameron King <cameronking42@gmail.com>
2024-09-25 15:20:19 -0400

    Fleshes out majority of common typography commands, barring weight & family selection.

--- src/app/commands/typography.ts
+++ src/app/commands/typography.ts
@@ -1,4 +1,4 @@
-import { clone, curr } from '../utils';
+import { clone, curr, preloadSelectedFonts } from '../utils';
 import { CmdGroup } from '../commands';
 
 const currText = () =>
@@ -10,7 +10,6 @@ export default {
     // these commands need *guarding*
     // they apply only to text nodes
     // should be available only when all selected nodes are text nodes
-    // exec() ideally should know that all commands
     t: {
         label: 'Typography',
         children: {
@@ -18,7 +17,8 @@ export default {
                 id: 'typography.size',
                 label: '(Set) Size',
                 args: [{ name: 'size', type: 'livenumber', label: 'Font Size', prop: 'fontSize' }],
-                exec({ size }) {
+                async exec({ size }) {
+                    await preloadSelectedFonts();
                     currText().forEach((node) => (node.fontSize = size));
                 },
             },
@@ -34,7 +34,8 @@ export default {
                         propKey: 'value',
                     },
                 ],
-                exec({ letterSpacing }) {
+                async exec({ letterSpacing }) {
+                    await preloadSelectedFonts();
                     currText().forEach(
                         (node) => (node.letterSpacing = clone(node.letterSpacing, { value: letterSpacing }))
                     );
@@ -52,13 +53,176 @@ export default {
                         propKey: 'value',
                     },
                 ],
-                exec({ lineHeight }) {
+                async exec({ lineHeight }) {
+                    await preloadSelectedFonts();
                     currText().forEach((node) => {
                         node.lineHeight = {
                             unit: 'PIXELS',
                             value: lineHeight,
                         };
                     });
+                },
+            },
+            ps: {
+                id: 'typography.paragraph_spacing',
+                label: '(Set) Paragraph Spacing',
+                args: [
+                    {
+                        name: 'paragraphSpacing',
+                        type: 'livenumber',
+                        label: 'Paragraph Spacing',
+                        prop: 'paragraphSpacing',
+                    },
+                ],
+                async exec({ paragraphSpacing }) {
+                    await preloadSelectedFonts();
+                    currText().forEach((node) => (node.paragraphSpacing = paragraphSpacing));
+                },
+            },
+            a: {
+                label: 'Align',
+                children: {
+                    l: {
+                        id: 'typography.align_left',
+                        label: 'Left',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignHorizontal = 'LEFT'));
+                        },
+                    },
+                    c: {
+                        id: 'typography.align_center',
+                        label: 'Center',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignHorizontal = 'CENTER'));
+                        },
+                    },
+                    r: {
+                        id: 'typography.align_right',
+                        label: 'Right',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignHorizontal = 'RIGHT'));
+                        },
+                    },
+                    j: {
+                        id: 'typography.align_justified',
+                        label: 'Justified',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignHorizontal = 'JUSTIFIED'));
+                        },
+                    },
+                    t: {
+                        id: 'typography.align_top',
+                        label: 'Top',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignVertical = 'TOP'));
+                        },
+                    },
+                    m: {
+                        id: 'typography.align_middle',
+                        label: 'Middle (Vertical)',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignVertical = 'CENTER'));
+                        },
+                    },
+                    b: {
+                        id: 'typography.align_bottom',
+                        label: 'Align Bottom',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textAlignVertical = 'BOTTOM'));
+                        },
+                    },
+                },
+            },
+            aw: {
+                id: 'typography.auto_width',
+                label: 'Auto Width',
+                async exec() {
+                    await preloadSelectedFonts();
+                    currText().forEach((node) => (node.textAutoResize = 'WIDTH_AND_HEIGHT'));
+                },
+            },
+            ah: {
+                id: 'typography.auto_height',
+                label: 'Auto Height',
+                async exec() {
+                    await preloadSelectedFonts();
+                    currText().forEach((node) => (node.textAutoResize = 'HEIGHT'));
+                },
+            },
+            fs: {
+                id: 'typography.fixed_size',
+                label: 'Fixed Size',
+                async exec() {
+                    await preloadSelectedFonts();
+                    currText().forEach((node) => (node.textAutoResize = 'NONE'));
+                },
+            },
+            t: {
+                id: 'typography.truncate',
+                label: 'Truncate',
+                async exec() {
+                    await preloadSelectedFonts();
+                    currText().forEach((node) => (node.textAutoResize = 'TRUNCATE'));
+                },
+            },
+            c: {
+                label: 'Case',
+                children: {
+                    o: {
+                        id: 'typography.case.original',
+                        label: 'Original',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textCase = 'ORIGINAL'));
+                        },
+                    },
+                    u: {
+                        id: 'typography.case.upper',
+                        label: 'Upper',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textCase = 'UPPER'));
+                        },
+                    },
+                    l: {
+                        id: 'typography.case.lower',
+                        label: 'Lower',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textCase = 'LOWER'));
+                        },
+                    },
+                    t: {
+                        id: 'typography.case.title',
+                        label: 'Title',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textCase = 'TITLE'));
+                        },
+                    },
+                    sc: {
+                        id: 'typography.case.small_caps',
+                        label: 'Small Caps',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textCase = 'SMALL_CAPS'));
+                        },
+                    },
+                    scf: {
+                        id: 'typography.case.small_caps_forced',
+                        label: 'Small Caps - Forced',
+                        async exec() {
+                            await preloadSelectedFonts();
+                            currText().forEach((node) => (node.textCase = 'SMALL_CAPS_FORCED'));
+                        },
+                    },
                 },
             },
         },
